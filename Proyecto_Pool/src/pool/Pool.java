f2578cb5b5e13af8e2a1c44627dde487f9adacdd
JDGiovanni <146672729+JDGiovanni@users.noreply.github.com>
2026-06-08 03:22:09 +0000

    cambio pool manager

--- Proyecto_Pool/src/pool/Pool.java
+++ Proyecto_Pool/src/pool/Pool.java
@@ -1,5 +1,43 @@
 package pool;
 
+import model.Connection;
+import model.PgConnection;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Pool {
-    
+    private static Pool instance;
+    private final BlockingQueue<Connection> availableConnections;
+    private int maxConnections;
+
+    private Pool() {
+        this.availableConnections = new LinkedBlockingQueue<>();
+    }
+
+    public static synchronized Pool getInstance() {
+        if (instance == null) {
+            instance = new Pool();
+        }
+        return instance;
+    }
+
+    public Connection getConn() {
+        return availableConnections.poll();
+    }
+
+    public void releaseConn(Connection conn) {
+        if(conn != null) {
+            availableConnections.offer(conn);
+        }
+    }
+
+    protected void addConnection(Connection conn) {
+        if(conn != null) {
+            availableConnections.offer(conn);
+        }
+    }
+
+    public int getAvailableConnections() {
+        return availableConnections.size();
+    }
 }
--- Proyecto_Pool/src/pool/PoolManager.java
+++ Proyecto_Pool/src/pool/PoolManager.java
@@ -1,5 +1,62 @@
 package pool;
 
+import model.Connection;
+import model.PgConnection;
 public class PoolManager {
-    
+    private final Pool pool;
+    private final int minSize;
+    private final int maxSize;
+
+    public PoolManager(int minSize, int maxSize) {
+        this.pool = Pool.getInstance();
+        this.minSize = minSize;
+        this.maxSize = maxSize;
+
+        initPool();
+    }
+
+    private void initPool() {
+        for (int i = 0; i < minSize; i++) {
+            pool.addConnection(new PgConnection());
+        }
+        IO.print("Pool inicializado con " + minSize + " conexiones.");
+    }
+
+    public Connection getConn() {
+        Connection conn = pool.getConn();
+
+        if (conn == null) {
+            synchronized (this) {
+                IO.print("No hay conexiones libres, incrementando cantidad");
+                grow();
+                conn = pool.getConn();
+            }
+        }
+        return conn;
+    }
+
+    public void releaseConn(Connection conn) {
+        pool.releaseConn(conn);
+
+        if (pool.getAvailableConnections() > minSize) {
+            shrink();
+        }
+    }
+
+    public void grow() {
+        pool.addConnection(new PgConnection());
+    }
+
+    public synchronized void shrink() {
+        Connection connToRemove = getConn();
+
+        while (pool.getAvailableConnections() > minSize) {
+            if (connToRemove != null) {
+                IO.print("Eliminando conexion para ahorrar memoria");
+                connToRemove.close();
+            } else {
+                break;
+            }
+        }
+    }
 }

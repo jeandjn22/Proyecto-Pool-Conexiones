c4159318e436d217e5e2ef0496970bad5c0fe5a3
DaimarPP <daimarpenso@gmail.com>
2026-06-07 23:04:28 -0400

    refactor: change Connection abstract class to interface with lifecycle methods

--- Proyecto_Pool/src/model/Connection.java
+++ Proyecto_Pool/src/model/Connection.java
@@ -1,19 +1,7 @@
 package model;
 
-public abstract class Connection {
-    protected int retries;
-    protected int timeout;
-
-    // Constructor base para que herede cualquier base de datos
-    public Connection(int retries, int timeout) {
-        this.retries = retries;
-        this.timeout = timeout;
-    }
-
-    // El método polimórfico que usarán tus compañeros en los hilos
-    public abstract void query();
-
-    // Getters por si el Pool o el Simulador necesitan consultar las propiedades
-    public int getRetries() { return retries; }
-    public int getTimeout() { return timeout; }
+public interface Connection {
+    void connect();
+    void query(String sql);
+    void close();
 }
